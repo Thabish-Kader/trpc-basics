4c2cf792c65b64085f31385d0ba0316485bfaac6
Thabish <kadertabish2@gmail.com>
2023-01-28 11:19:56 +0400

    inital-config
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+const MyApp: AppType = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
+export default trpc.withTRPC(MyApp);
